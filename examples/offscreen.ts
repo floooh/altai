0fefa904651cda054b487820f23b637210516714
Andre Weissflog <floooh@gmail.com>
2017-04-08 15:03:02 +0200

    More interesting offscreen-rendering example

--- examples/offscreen.ts
+++ examples/offscreen.ts
@@ -1,9 +1,10 @@
 /// <reference path="../src/altai.ts"/>
+/// <reference path="math.ts"/>
 
 function offscreen() {
+    // setup the GL canvas
     const WIDTH = 600;
     const HEIGHT = 400;
-
     let gfx = new altai.Gfx({ 
         UseWebGL2: true, 
         Width: WIDTH, 
@@ -13,10 +14,10 @@ function offscreen() {
     });
 
     // create texture and render-pass for offscreen-rendering
-    let tex = gfx.makeTexture({
+    let offTex = gfx.makeTexture({
         Type: altai.TextureType.Texture2D,
-        Width: 128,
-        Height: 128,
+        Width: 256,
+        Height: 256,
         ColorFormat: altai.PixelFormat.RGBA8,
         DepthFormat: altai.DepthStencilFormat.DEPTH,
         WrapU: altai.Wrap.Repeat,
@@ -25,27 +26,118 @@ function offscreen() {
         MagFilter: altai.Filter.Linear, 
     });
     let offPass = gfx.makePass({
-        ColorAttachments: [{ Texture: tex, ClearColor: [0.0, 0.0, 1.0, 1.0] }],
-        DepthAttachment: { Texture: tex }
+        ColorAttachments: [{ Texture: offTex, ClearColor: [0.25, 0.25, 0.25, 1.0] }],
+        DepthAttachment: { Texture: offTex }
     });
 
-    // default pass
+    // a vertex and index buffer for a cube
+    let cubeVB = gfx.makeBuffer({
+        Type: altai.BufferType.VertexBuffer,
+        Data: new Float32Array([
+            -1.0, -1.0, -1.0,   1.0, 0.0, 0.0, 1.0, 
+             1.0, -1.0, -1.0,   1.0, 0.0, 0.0, 1.0,
+             1.0,  1.0, -1.0,   1.0, 0.0, 0.0, 1.0,
+            -1.0,  1.0, -1.0,   1.0, 0.0, 0.0, 1.0,
+
+            -1.0, -1.0,  1.0,   0.0, 1.0, 0.0, 1.0,      
+             1.0, -1.0,  1.0,   0.0, 1.0, 0.0, 1.0, 
+             1.0,  1.0,  1.0,   0.0, 1.0, 0.0, 1.0,
+            -1.0,  1.0,  1.0,   0.0, 1.0, 0.0, 1.0,
+
+            -1.0, -1.0, -1.0,   0.0, 0.0, 1.0, 1.0, 
+            -1.0,  1.0, -1.0,   0.0, 0.0, 1.0, 1.0, 
+            -1.0,  1.0,  1.0,   0.0, 0.0, 1.0, 1.0, 
+            -1.0, -1.0,  1.0,   0.0, 0.0, 1.0, 1.0,
+
+             1.0, -1.0, -1.0,   1.0, 1.0, 0.0, 1.0, 
+             1.0,  1.0, -1.0,   1.0, 1.0, 0.0, 1.0, 
+             1.0,  1.0,  1.0,   1.0, 1.0, 0.0, 1.0, 
+             1.0, -1.0,  1.0,   1.0, 1.0, 0.0, 1.0,
+
+            -1.0, -1.0, -1.0,   0.0, 1.0, 1.0, 1.0, 
+            -1.0, -1.0,  1.0,   0.0, 1.0, 1.0, 1.0, 
+             1.0, -1.0,  1.0,   0.0, 1.0, 1.0, 1.0, 
+             1.0, -1.0, -1.0,   0.0, 1.0, 1.0, 1.0,
+
+            -1.0,  1.0, -1.0,   1.0, 0.0, 1.0, 1.0, 
+            -1.0,  1.0,  1.0,   1.0, 0.0, 1.0, 1.0, 
+             1.0,  1.0,  1.0,   1.0, 0.0, 1.0, 1.0, 
+             1.0,  1.0, -1.0,   1.0, 0.0, 1.0, 1.0             
+        ])
+    })
+    let cubeIB = gfx.makeBuffer({
+        Type: altai.BufferType.IndexBuffer,
+        Data: new Uint16Array([
+            0, 1, 2,  0, 2, 3,  
+            4, 5, 6,  4, 6, 7,
+            8, 9, 10,  8, 10, 11,  
+            12, 13, 14,  12, 14, 15,
+            16, 17, 18,  16, 18, 19,  
+            20, 21, 22,  20, 22, 23            
+        ])
+    })
+
+    // a shader to render a vertex-colored cube
+    let cubeShader = gfx.makeShader({
+        VertexShader: `
+            uniform mat4 mvp;
+            attribute vec4 position;
+            attribute vec4 color;
+            varying lowp vec4 vColor;
+            void main(void) {
+                gl_Position = mvp * position;
+                vColor = color;
+            }`,
+        FragmentShader: `
+            varying lowp vec4 vColor;
+            void main(void) {
+                gl_FragColor = vColor;
+            }`
+    });
+
+    // a pipeline object to render the cube
+    let cubePipeline = gfx.makePipeline({
+        Shader: cubeShader,
+        VertexLayouts: [{
+            Components: [
+                [ "position", altai.VertexFormat.Float3 ],
+                [ "color", altai.VertexFormat.Float4 ],
+            ]
+        }],
+        IndexFormat: altai.IndexFormat.UInt16,
+        DepthCmpFunc: altai.CompareFunc.LessEqual,
+        DepthWriteEnabled: true,
+        CullFaceEnabled: false
+    });
+
+    // draw state with resource bindings for the cube
+    let cubeDrawState = gfx.makeDrawState({
+        Pipeline: cubePipeline,
+        IndexBuffer: cubeIB,
+        VertexBuffers: [ cubeVB ],
+    });
+
+    // default render pass
     let pass = gfx.makePass({
-        ColorAttachments: [{ ClearColor: [0.4, 0.6, 0.5, 1.0] }]
+        ColorAttachments: [{ ClearColor: [0.75, 0.75, 0.75, 1.0] }]
     });
 
-    // a vertex buffer for a textured triangle
-    let vertexBuffer = gfx.makeBuffer({
+    // a vertex buffer for a textured quad
+    let quadVB = gfx.makeBuffer({
         Type: altai.BufferType.VertexBuffer,
         Data: new Float32Array([
             // positions        texcoords
-            0.0, 0.5, 0.5,      0.0, 0.0,
-            0.5, -0.5, 0.5,     1.0, 0.0,
-            -0.5, -0.5, 0.5,    1.0, 1.0,
+            -0.5, -0.5, 0.5,    0.0, 0.0,   // first triangle
+            +0.5, -0.5, 0.5,    2.0, 0.0,
+            +0.5, +0.5, 0.5,    2.0, 2.0,
+
+            -0.5, -0.5, 0.5,    0.0, 0.0,   // second triangle
+            +0.5, +0.5, 0.5,    2.0, 2.0,
+            -0.5, +0.5, 0.5,    0.0, 2.0,
         ]),
     });
 
-    // simple shader for rendering a textured triangle
+    // shader to render a textured quad into the default framebuffer
     let shader = gfx.makeShader({
         VertexShader: `
             attribute vec4 position;
@@ -64,7 +156,7 @@ function offscreen() {
             }`
     });
 
-    // the pipeline-state object 
+    // the pipeline-state object for the textured quad
     let pipeline = gfx.makePipeline({
         VertexLayouts: [{
             Components: [
@@ -78,23 +170,42 @@ function offscreen() {
         CullFaceEnabled: false,
     });
 
-    // DrawState object with the resource bindings
+    // DrawState object for the texture quad
     let drawState = gfx.makeDrawState({
         Pipeline: pipeline,
-        VertexBuffers: [ vertexBuffer ],
+        VertexBuffers: [ quadVB ],
         Textures: {
-            "texture": tex
+            "texture": offTex,
         }
     });
+    
+    // rotation angles and projection matrix
+    let angleX = 0.0;
+    let angleY = 0.0;
+    const proj = mat4.perspective_fov(deg2rad(45.0), WIDTH, HEIGHT, 0.01, 100.0);
     function draw() {
-        // offscreen rendering
+        
+        // model-view-projection matrix for rotating cube
+        angleX += 0.01;
+        angleY += 0.02;
+        let model = mat4.translate(mat4.identity(), 0.0, 0.0, -6.0);
+        model = mat4.rotate(model, angleX, 1.0, 0.0, 0.0);
+        model = mat4.rotate(model, angleY, 0.0, 1.0, 0.0);
+        let mvp = mat4.mul(proj, model);
+
+        // render rotating cube to offscreen render target
         gfx.beginPass(offPass);
+        gfx.applyDrawState(cubeDrawState);
+        gfx.applyUniforms({
+            "mvp": mvp.values()
+        });
+        gfx.draw(0, 36);
         gfx.endPass();
         
         // render to default framebuffer
         gfx.beginPass(pass);
         gfx.applyDrawState(drawState);
-        gfx.draw(0, 3);
+        gfx.draw(0, 6);
         gfx.endPass();
         gfx.commitFrame(draw);
     }
